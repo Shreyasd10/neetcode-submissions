ee540ccd6a17077116745b238c3121b1f01db238
Shreyas <52241691+Shreyasd10@users.noreply.github.com>
2026-06-05 23:02:56 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       HashMap<String,List<String>> map = new HashMap<>();
+
+       for(String word :  strs){
+
+           int[] count = new int[26];
+           for( char c : word.toCharArray()){
+                count[c - 'a']++;
+           }
+           String key  = Arrays.toString(count);
+           map.computeIfAbsent(key, k -> new ArrayList<>()).add(word);
+       }
+       
+       return new ArrayList<>(map.values());
+    }
+}
